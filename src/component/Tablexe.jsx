8758f86f83ac936a4be6ba273200fd82841a4b7e
vaibhav7604 <vaibhavmeghani79@gmail.com>
2025-01-29 15:09:35 +0530

    changes

--- src/component/Tablexe.jsx
+++ src/component/Tablexe.jsx
@@ -3,8 +3,22 @@ import datacenter from "../assets/images/datacenter.jpg";
 import dummy from "../assets/images/dummy.png";
 import dummy2 from "../assets/images/dummy2.png";
 import "./Tablexe.css";
+import { motion } from "framer-motion";
 
 function Tablexe() {
+  // motion function animation
+  const HeadingAnimation = {
+    initialProp: { opacity: 0, y: -50 },
+    whileInViewProp: { opacity: 1, y: 0 },
+    transitionProp: {
+      duration: 1,
+      delay: 0.3,
+      type: "spring",
+      stiffness: 100,
+      damping: 10,
+    },
+  };
+
   const images = [dummy, dummy2, dummy, dummy2];
   const [selectedRow, setSelectedRow] = useState(1);
 
@@ -14,7 +28,14 @@ function Tablexe() {
 
   return (
     <>
-      <h2 id="Heading-table">How Does it Work</h2>
+      <motion.h2
+        id="Heading-table"
+        initial={HeadingAnimation.initialProp}
+        whileInView={HeadingAnimation.whileInViewProp}
+        transition={HeadingAnimation.transitionProp}
+      >
+        How Does it Work
+      </motion.h2>
       <div className="Tablexe-container">
         <div className="table-container">
           <table>
@@ -40,12 +61,29 @@ function Tablexe() {
           )}
         </div>
       </div>
-      <div className="showepic">
+      <motion.div
+        initial={{ clipPath: "inset(100% 50% 50% 50%)" }}
+        whileInView={{ clipPath: "inset(0% 0 0 0)" }}
+        transition={{ duration: 0.8, ease: "easeIn" }}
+        className="showepic"
+      >
         <div className="img-text">
-          <p id="textofimg">Some text here dummy dummy..!!</p>
+          <motion.p
+            initial={{ filter: "blur(1px)" }}
+            whileHover={{ skewX: 2, skewY: 2, filter: "blur(0)", scale: 1.1 }}
+            id="textofimg"
+          >
+            Some text here dummy dummy..!!
+          </motion.p>
         </div>
-        <img src={datacenter} alt="some img here" />
-      </div>
+        <motion.img
+          initial={{ opacity: 1 }}
+          whileHover={{ filter: "blur(2px)" }}
+          transition={{ duration: 0.3 }}
+          src={datacenter}
+          alt="some img here"
+        />
+      </motion.div>
     </>
   );
 }
